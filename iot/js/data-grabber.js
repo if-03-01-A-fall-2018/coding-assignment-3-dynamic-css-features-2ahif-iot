fef2e903bf82d78c96b310070eb8434e94b8f6da
Wallinger Marc <if170115@WS8A08.htl-leonding.ac.at>
2019-04-08 08:51:03 +0200

    Replaced jsonGet with FetchAPI

--- iot/js/data-grabber.js
+++ iot/js/data-grabber.js
@@ -1,9 +1,41 @@
 function runDataGrabber() {
-    var heatingData = jQuery.getJSON("https://www.wallinger-online.at/heating/", null, replaceData);
-    var logData = jQuery.get("https://www.wallinger-online.at/heating/log.php", null, parseData);
+  var heatingData;
+  var logData;
+
+     fetch('https://www.wallinger-online.at/heating/')
+     .then(response => {
+         if (!response.ok) {
+             throw new Error("HTTP error " + response.status);
+         }
+         return response.json();
+     })
+     .then(json => {
+         heatingData = json;
+         console.log(heatingData);
+         replaceData();
+     })
+     .catch(function () {
+         this.dataError = true;
+     });
+
+     fetch('https://www.wallinger-online.at/heating/log.php')
+     .then(response => {
+         if (!response.ok) {
+             throw new Error("HTTP error " + response.status);
+         }
+         return response.text();
+     })
+     .then(text => {
+         logData = text;
+         console.log(logData);
+         parseData();
+     })
+     .catch(function () {
+         this.dataError = true;
+     });
 
     function parseData() {
-      var logDataArray = logData.responseText.split('\n');
+      var logDataArray = logData.split('\n');
       for (var i = logDataArray.length - 50; i < logDataArray.length - 1; i++) {
         var currLine = logDataArray[i].split(' ');
         var dateString = currLine[0].replace('_', 'T');
@@ -15,23 +47,23 @@ function runDataGrabber() {
     }
 
     function replaceData() {
-        document.getElementById("outside-temp").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Aussen"].Value + " °C";
-        document.getElementById("heating-flow").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Vorlauf"].Value + " °C";
-        document.getElementById("return-flow").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Ruecklauf"].Value + " °C";
-        document.getElementById("return-flow-set").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Ruecklauf-Soll"].Value + " °C";
-        document.getElementById("warm-water").innerHTML = heatingData.responseJSON.Results[0].Readings["Ww-Temp"].Value + " °C";
-        document.getElementById("evaporator").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Verdampfer"].Value + " °C";
-        document.getElementById("condenser").innerHTML = heatingData.responseJSON.Results[0].Readings["Temp-Kondensator"].Value + " °C";
+        document.getElementById("outside-temp").innerHTML = heatingData.Results[0].Readings["Temp-Aussen"].Value + " °C";
+        document.getElementById("heating-flow").innerHTML = heatingData.Results[0].Readings["Temp-Vorlauf"].Value + " °C";
+        document.getElementById("return-flow").innerHTML = heatingData.Results[0].Readings["Temp-Ruecklauf"].Value + " °C";
+        document.getElementById("return-flow-set").innerHTML = heatingData.Results[0].Readings["Temp-Ruecklauf-Soll"].Value + " °C";
+        document.getElementById("warm-water").innerHTML = heatingData.Results[0].Readings["Ww-Temp"].Value + " °C";
+        document.getElementById("evaporator").innerHTML = heatingData.Results[0].Readings["Temp-Verdampfer"].Value + " °C";
+        document.getElementById("condenser").innerHTML = heatingData.Results[0].Readings["Temp-Kondensator"].Value + " °C";
 
-        document.getElementById("mode").innerHTML = heatingData.responseJSON.Results[0].Readings["Betriebs-Mode"].Value;
-        document.getElementById("cpu-date").innerHTML = heatingData.responseJSON.Results[0].Readings["CPU-Boot-Datum"].Value;
-        document.getElementById("cpu-time").innerHTML = heatingData.responseJSON.Results[0].Readings["CPU-Boot-Zeit"].Value;
-        document.getElementById("ht-inc-off").innerHTML = heatingData.responseJSON.Results[0].Readings["Hz-Anhebung-Aus"].Value;
-        document.getElementById("ht-inc-on").innerHTML = heatingData.responseJSON.Results[0].Readings["Hz-Anhebung-Ein"].Value;
-        document.getElementById("ht-hyst").innerHTML = heatingData.responseJSON.Results[0].Readings["Hz-Hysterese"].Value;
-        document.getElementById("compr-hours").innerHTML = heatingData.responseJSON.Results[0].Readings["KomprBetrStunden"].Value;
+        document.getElementById("mode").innerHTML = heatingData.Results[0].Readings["Betriebs-Mode"].Value;
+        document.getElementById("cpu-date").innerHTML = heatingData.Results[0].Readings["CPU-Boot-Datum"].Value;
+        document.getElementById("cpu-time").innerHTML = heatingData.Results[0].Readings["CPU-Boot-Zeit"].Value;
+        document.getElementById("ht-inc-off").innerHTML = heatingData.Results[0].Readings["Hz-Anhebung-Aus"].Value;
+        document.getElementById("ht-inc-on").innerHTML = heatingData.Results[0].Readings["Hz-Anhebung-Ein"].Value;
+        document.getElementById("ht-hyst").innerHTML = heatingData.Results[0].Readings["Hz-Hysterese"].Value;
+        document.getElementById("compr-hours").innerHTML = heatingData.Results[0].Readings["KomprBetrStunden"].Value;
 
-        document.getElementById("last-request").innerHTML = heatingData.responseJSON.Results[0].Readings["Zeit"].Value;
+        document.getElementById("last-request").innerHTML = heatingData.Results[0].Readings["Zeit"].Value;
     }
 }
 
